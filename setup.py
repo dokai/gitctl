57bd8641c726cd4ff9b1fa65252ff73cf84147ad
Kai Lautaportti <kai.lautaportti@hexagonit.fi>
2008-05-30 12:47:09 +0300

    Updated metadata.

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
 from setuptools import setup, find_packages
 
-version = '0.1'
+version = '1.0a1'
 
 setup(name='gitctl',
       version=version,
       description="Script to manage multiple Git repositories in a manner similar to svn:externals.",
       long_description="""\
 """,
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Version Control',
+        'Topic :: Software Development :: Build Tools',
+        ], # Get strings from http://pypi.python.org/pypi?:action=list_classifiers
       keywords='git',
       author='Kai Lautaportti',
       author_email='kai.lautaportti@hexagonit.fi',
