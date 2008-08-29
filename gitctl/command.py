a28189baf91915b07459fa869fa1d9bd78f4f7fe
Kai Lautaportti <kai.lautaportti@hexagonit.fi>
2008-08-29 17:04:02 +0300

    Refactored the helper functions into gitctl.utils.

--- gitctl/command.py
+++ gitctl/command.py
@@ -3,121 +3,19 @@
 import os
 import sys
 import git
-import shlex
 import logging
-import subprocess
 
-from StringIO import StringIO
-from ConfigParser import SafeConfigParser
+import gitctl.utils
 
 logging.basicConfig(level=logging.INFO, format='%(message)s')
 LOG = logging.getLogger()
 
-def project_path(proj, relative=False):
-    """Returns the absolute project path unless relative=True, when a path
-    relative to the current directory will be returned.
-    """
-    path = os.path.realpath(
-        os.path.abspath(os.path.join(proj['container'], proj['name'])))
-    if relative:
-        prefix_len = len(os.path.commonprefix(os.path.realpath(os.getcwd()), path)) + 1
-        path = path[prefix_len:]
-    return path
-
-def clean_working_dir(repository):
-    """Returns True if the given repository has no uncommited changes."""
-    g = isinstance(repository, git.Git) and repository or repository.git
-    return len(g.diff().strip()) == 0 and \
-           len(g.diff('--cached').strip()) == 0
-
-def run(command, cwd=None):
-    """Executes the given command."""
-    if hasattr(command, 'startswith'):
-        # Split the command into tokens, honoring any quoted parts
-        lexer = shlex.shlex(command)
-        lexer.whitespace_split = True
-        command = list(lexer)
-
-    pipe = subprocess.Popen(command, cwd=cwd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    retcode = pipe.wait()
-    
-    return retcode, pipe.stdout.read(), pipe.stderr.read()
-
-def parse_config(config):
-    """Parses the gitctl config file."""
-    parser = SafeConfigParser({'upstream' : 'origin'})
-    if len(parser.read([config, os.path.expanduser('~/.gitctl.cfg')])) == 0:
-        raise ValueError('Invalid config file: %s' % config)
-    
-    if not parser.has_section('gitctl'):
-        raise ValueError('The [gitctl] section is missing')
-    
-    upstream = parser.get('gitctl', 'upstream')
-    return {'upstream' : upstream,
-            'upstream-url' : parser.get('gitctl', 'upstream-url'),
-            'commit-email' : parser.get('gitctl', 'commit-email'),
-            'commit-email-prefix' : parser.get('gitctl', 'commit-email-prefix'),
-            'branches' : [('%s/%s' % (upstream, branch), branch)
-                          for branch
-                          in parser.get('gitctl', 'branches').split()],
-            'staging-branch' : '%s/%s' % (upstream, parser.get('gitctl', 'staging-branch')),
-            'development-branch' : parser.get('gitctl', 'development-branch'),
-            }
-
-def parse_externals(config):
-    """Parses the gitctl externals configuration."""
-    parser = SafeConfigParser({'type' : 'git'})
-    if len(parser.read(config)) == 0:
-        LOG.critical('Invalid externals configuration: %s', config)
-        sys.exit(1)
-
-    projects = []
-    for sec in parser.sections():
-        if not parser.has_option(sec, 'url'):
-            LOG.critical('Section %s is missing the ``url`` option in the externals configuration', sec)
-            sys.exit(1)
-       
-        proj = {
-           'name' : sec.strip(),
-           'url' : parser.get(sec, 'url').strip(),
-           'type' : parser.get(sec, 'type').strip() }
-        
-        if parser.has_option(sec, 'container'):
-            proj['container'] = parser.get(sec, 'container').strip()
-
-        if proj['type'] not in ('git', 'git-svn'):
-            raise ValueError('Invalid type: %s. Supported types are "git" and "git-svn".' % proj['type'])
-
-        if proj['type'] == 'git':
-            proj['treeish'] = parser.get(sec, 'treeish').strip()
-        elif proj['type'] == 'git-svn':
-            for opt in 'svn-trunk', 'svn-tags', 'svn-branches':
-                if parser.has_option(sec, opt):
-                    proj[opt] = parser.get(sec, opt).strip()
-            if parser.has_option(sec, 'svn-clone-options'):
-                proj['svn-clone-options'] = parser.get(sec, 'svn-clone-options').split()
-
-        projects.append(proj)
-    
-    return projects
-
-def generate_externals(projects):
-    """Generates an externals configuration file."""
-    config = SafeConfigParser()
-    for project in projects:
-        section = project.pop('name')
-        config.add_section(section)
-        for k, v in project.iteritems():
-            config.set(section, k, v)
-    buf = StringIO()
-    config.write(buf)
-    return buf.getvalue()
     
 def gitctl_create(args):
     """Handles the 'gitctl create' command"""
     project_path = os.path.realpath(os.path.join(os.getcwd(), args.project[0]))
     project_name = os.path.basename(project_path)
-    config = parse_config(args.config)
+    config = gitctl.utils.parse_config(args.config)
     
     if not os.path.exists(project_path):
         LOG.critical('Project path does not exist!', project_path)
@@ -126,7 +24,7 @@ def gitctl_create(args):
     project_url = '%s:%s.git' % (config['upstream-url'], project_name)
 
     # Make sure that the remote repository does not exist already.
-    retcode = run('ssh %s "test ! -d %s.git"' % (config['upstream-url'], project_name))
+    retcode = gitctl.utils.run('ssh %s "test ! -d %s.git"' % (config['upstream-url'], project_name))
     if retcode != 0:
         LOG.error('Remote repository ``%s`` already exists. Aborting.', project_url)
         sys.exit(1)
@@ -146,7 +44,7 @@ def gitctl_create(args):
             'commit_email' : config['commit-email'],
             'commit_email_prefix' : config['commit-email-prefix'] },
         )
-    run(initialize_remote)
+    gitctl.utils.run(initialize_remote)
     LOG.info('Created new remote repository: %s', project_url)
     
     # Initialize the local directory.
@@ -183,12 +81,12 @@ def gitctl_create(args):
 
 def gitctl_fetch(args):
     """Fetches all projects."""
-    projects = parse_externals(args.externals)
-    config = parse_config(args.config)
+    projects = gitctl.utils.parse_externals(args.externals)
+    config = gitctl.utils.parse_config(args.config)
     
     LOG.info('Fetching projects..')
     for proj in projects:
-        repository = git.Git(project_path(proj))
+        repository = git.Git(gitctl.utils.project_path(proj))
         repository.fetch(config['upstream'])
         LOG.info(proj['name'])
 
@@ -198,15 +96,15 @@ def gitctl_update(args):
     If the project already exists locally, it will be pulled (or rebased).
     Otherwise it will cloned.
     """
-    projects = parse_externals(args.externals)
-    config = parse_config(args.config)
+    projects = gitctl.utils.parse_externals(args.externals)
+    config = gitctl.utils.parse_config(args.config)
     
     LOG.info('Updating projects..')
     for proj in projects:
-        path = project_path(proj)
+        path = gitctl.utils.project_path(proj)
         if os.path.exists(path):
             repository = git.Git(path)
-            if not clean_working_dir(repository):
+            if gitctl.utils.is_dirty(repository):
                 print proj['name'], 'has local changes. Please commit or stash them and try again.'
                 continue
 
@@ -233,17 +131,17 @@ def gitctl_update(args):
 
 def gitctl_status(args):
     """Checks the status of all external projects."""
-    config = parse_config(args.config)
-    projects = parse_externals(args.externals)
+    config = gitctl.utils.parse_config(args.config)
+    projects = gitctl.utils.parse_externals(args.externals)
 
     LOG.info('Checking status..')
     for proj in projects:
-        repository = git.Repo(project_path(proj))
+        repository = git.Repo(gitctl.utils.project_path(proj))
         if not args.no_fetch:
             # Fetch upstream
             repository.git.fetch(config['upstream'])
 
-        if not clean_working_dir(repository):
+        if gitctl.utils.is_dirty(repository):
             LOG.info('%s Uncommitted local changes', proj['name'].ljust(30, '.'))
             continue
             
@@ -268,12 +166,12 @@ def gitctl_changes(args):
     reported differences are the ones most likely pending to be pushed into
     production.
     """
-    projects = parse_externals(args.externals)
-    config = parse_config(args.config)
+    projects = gitctl.utils.parse_externals(args.externals)
+    config = gitctl.utils.parse_config(args.config)
 
     LOG.info('Comparing changes to pinned down versions..')
     for proj in projects:
-        repository = git.Git(project_path(proj))
+        repository = git.Git(gitctl.utils.project_path(proj))
         
         remote_branches = set([name.strip()
                                for name
@@ -306,7 +204,7 @@ def gitctl_changes(args):
                 LOG.info('%s .. OK', proj['name'])
         
     if args.show_config:
-        print generate_externals(projects)
+        print gitctl.utils.generate_externals(projects)
 
 __all__ = ['gitctl_create', 'gitctl_fetch', 'gitctl_update', 'gitctl_status',
            'gitctl_changes',]
---
+++ gitctl/utils.py
@@ -0,0 +1,113 @@
+# -*- encoding: utf-8 -*-
+"""Common utilities for gitctl."""
+
+import os
+import git
+import shlex
+import subprocess
+
+from StringIO import StringIO
+from ConfigParser import SafeConfigParser
+
+
+def project_path(proj, relative=False):
+    """Returns the absolute project path unless relative=True, when a path
+    relative to the current directory will be returned.
+    """
+    path = os.path.realpath(
+        os.path.abspath(os.path.join(proj['container'], proj['name'])))
+    if relative:
+        prefix_len = len(os.path.commonprefix(os.path.realpath(os.getcwd()), path)) + 1
+        path = path[prefix_len:]
+    return path
+
+def is_dirty(repository):
+    """Returns True if the given repository has uncommited changes either in
+    the working directory or the index.
+    """
+    g = isinstance(repository, git.Git) and repository or repository.git
+    return len(g.diff().strip()) > 0 or \
+           len(g.diff('--cached').strip()) > 0
+
+def run(command, cwd=None):
+    """Executes the given command."""
+    if hasattr(command, 'startswith'):
+        # Split the command into tokens, honoring any quoted parts
+        lexer = shlex.shlex(command)
+        lexer.whitespace_split = True
+        command = list(lexer)
+
+    pipe = subprocess.Popen(command, cwd=cwd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    retcode = pipe.wait()
+    
+    return retcode, pipe.stdout.read(), pipe.stderr.read()
+
+def parse_config(config):
+    """Parses the gitctl config file."""
+    parser = SafeConfigParser({'upstream' : 'origin'})
+    if len(parser.read([config, os.path.expanduser('~/.gitctl.cfg')])) == 0:
+        raise ValueError('Invalid config file: %s' % config)
+    
+    if not parser.has_section('gitctl'):
+        raise ValueError('The [gitctl] section is missing')
+    
+    upstream = parser.get('gitctl', 'upstream')
+    return {'upstream' : upstream,
+            'upstream-url' : parser.get('gitctl', 'upstream-url'),
+            'commit-email' : parser.get('gitctl', 'commit-email'),
+            'commit-email-prefix' : parser.get('gitctl', 'commit-email-prefix'),
+            'branches' : [('%s/%s' % (upstream, branch), branch)
+                          for branch
+                          in parser.get('gitctl', 'branches').split()],
+            'staging-branch' : '%s/%s' % (upstream, parser.get('gitctl', 'staging-branch')),
+            'development-branch' : parser.get('gitctl', 'development-branch'),
+            }
+
+def parse_externals(config):
+    """Parses the gitctl externals configuration."""
+    parser = SafeConfigParser({'type' : 'git'})
+    if len(parser.read(config)) == 0:
+        LOG.critical('Invalid externals configuration: %s', config)
+        sys.exit(1)
+
+    projects = []
+    for sec in parser.sections():
+        if not parser.has_option(sec, 'url'):
+            LOG.critical('Section %s is missing the ``url`` option in the externals configuration', sec)
+            sys.exit(1)
+       
+        proj = {
+           'name' : sec.strip(),
+           'url' : parser.get(sec, 'url').strip(),
+           'type' : parser.get(sec, 'type').strip() }
+        
+        if parser.has_option(sec, 'container'):
+            proj['container'] = parser.get(sec, 'container').strip()
+
+        if proj['type'] not in ('git', 'git-svn'):
+            raise ValueError('Invalid type: %s. Supported types are "git" and "git-svn".' % proj['type'])
+
+        if proj['type'] == 'git':
+            proj['treeish'] = parser.get(sec, 'treeish').strip()
+        elif proj['type'] == 'git-svn':
+            for opt in 'svn-trunk', 'svn-tags', 'svn-branches':
+                if parser.has_option(sec, opt):
+                    proj[opt] = parser.get(sec, opt).strip()
+            if parser.has_option(sec, 'svn-clone-options'):
+                proj['svn-clone-options'] = parser.get(sec, 'svn-clone-options').split()
+
+        projects.append(proj)
+    
+    return projects
+
+def generate_externals(projects):
+    """Generates an externals configuration file."""
+    config = SafeConfigParser()
+    for project in projects:
+        section = project.pop('name')
+        config.add_section(section)
+        for k, v in project.iteritems():
+            config.set(section, k, v)
+    buf = StringIO()
+    config.write(buf)
+    return buf.getvalue()
