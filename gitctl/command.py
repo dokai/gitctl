ac06e9dd7ae74790f42115cddc5efebd667d9b2b
Roman Susi <roman.susi@hexagonit.fi>
2008-12-30 09:50:25 +0200

    encoding -> coding to keep Emacs happy

--- gitctl/command.py
+++ gitctl/command.py
@@ -1,4 +1,4 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 """Command handlers."""
 import os
 import sys
--- gitctl/parser.py
+++ gitctl/parser.py
@@ -1,4 +1,4 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 """CLI command parsing."""
 
 import os
